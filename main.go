8ca8899566093c384cdf4e09a387cd441675f73e
Fr0zenBoy <th129513@gmail.com>
2023-04-02 22:31:47 -0300

    List tasks and some fix

--- main.go
+++ main.go
@@ -2,86 +2,24 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"os"
 
-	"github.com/Fr0zenBoy/docker-scheduler/scheduler"
 	"github.com/gin-gonic/gin"
-	"github.com/go-co-op/gocron"
+	"github.com/Fr0zenBoy/docker-scheduler/scheduler"
 )
 
-type payload struct {
-	TaskName string `json:"taskName"`
-	CronJob string `json:"cronJob"`
-	//TODO container stuffs
-}
-
-type outGetJobs struct {
-	Result []string
-}
-
-type ds struct {
- New *gocron.Scheduler
-}
-
-func (s ds)GetJobs(c *gin.Context) {
-	payload := outGetJobs{
-		Result: scheduler.GetCronJobs(s.New),
-	}
-
-	c.JSON(http.StatusOK, payload)
-}
-
-func (s ds)AddJob(c *gin.Context) {
-	body := payload{}
-	if err := c.ShouldBindJSON(&body); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest,
-			gin.H{
-				"error": err.Error(),
-				"message": "Invalid inputs, please check your inputs",
-			})
-		return
-	}
-
-	job, err := scheduler.AddCronJobs(s.New, body.CronJob, body.TaskName, func () {fmt.Println("funciona!")})
-	if err != nil {
-
-		c.AbortWithStatusJSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusAccepted, map[string]interface{}{
-		"isRunning": job.IsRunning(),
-		"name": job.Tags(),
-		"next run": job.NextRun(),
-	})
-	
-}
-
-func (s ds)DeleteJob(c *gin.Context) {
-	jobName := c.Param("jobname")
-
-	scheduler.DeleteCronJob(s.New, jobName)
-	c.AbortWithStatus(http.StatusOK)
-
-}
+var task = func() {}
 
 func main() {
-	s := ds {
-		New: scheduler.NewCron(),
-	} 
-	
+
+	s := scheduler.NewCron()
+
 	app := gin.New()
 
-	// api routes
-	app.GET("/api/jobs", s.GetJobs)
+	app.GET("/api/jobs", s.LetJobs)
 	app.POST("/api/jobs", s.AddJob)
-	app.DELETE("/api/jobs/:id", s.DeleteJob)
+	app.DELETE("/api/jobs/:jobname", s.DeleteJob)
 
-	// run api on port 9092
   if err := app.Run(":9092"); err != nil {
 		_, err = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
--- scheduler/cron.go
+++ scheduler/cron.go
@@ -2,17 +2,25 @@ package scheduler
 
 import (
 	"time"
+	"fmt"
 
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/go-co-op/gocron"
 )
 
-func NewCron() *gocron.Scheduler {
-	s := gocron.NewScheduler(time.Local)
-	s.TagsUnique()
-	return s 
+type cronScheduler struct {
+	*gocron.Scheduler
 }
 
-func AddCronJobs(s *gocron.Scheduler ,cron, taskName string, TaskFunc any) (*gocron.Job, error) {
+type payload struct {
+	TaskName string `json:"taskName"`
+	CronJob string `json:"cronJob"`
+	//TODO container stuffs
+}
+
+func addCronJobs(s *cronScheduler ,cron , taskName string, TaskFunc any) (*gocron.Job, error) {
 	job , err := s.Cron(cron).Tag(taskName).Do(TaskFunc)
 	if err != nil {
 		return nil, err
@@ -21,15 +29,72 @@ func AddCronJobs(s *gocron.Scheduler ,cron, taskName string, TaskFunc any) (*goc
 	return job, err
 }
 
-func GetCronJobs(s *gocron.Scheduler) []string {
-	var jobs []string
-	for _, tags := range s.Jobs() {
-		
-		return tags.Tags()
+func listCronJobs(s *cronScheduler) map[string]map[string]any {
+	jobs := make(map[string]map[string]any)
+	for _, job := range s.Jobs() {
+		jobs[job.Tags()[0]] = make(map[string]any)
+		jobs[job.Tags()[0]]["isRunning"] = job.IsRunning()
+		jobs[job.Tags()[0]]["lastRun"] = job.LastRun()
+		jobs[job.Tags()[0]]["nextRun"] = job.NextRun()
+		jobs[job.Tags()[0]]["runCount"] = job.RunCount()
+		jobs[job.Tags()[0]]["scheduledTime"] = job.ScheduledTime()
 	}
 	return jobs
 }
 
-func DeleteCronJob(s *gocron.Scheduler, tags ...string) error {
+func deleteCronJob(s *cronScheduler, tags ...string) error {
 	return s.RemoveByTags(tags...)
+}
+
+func NewCron() *cronScheduler {
+	gs:= &cronScheduler{
+		gocron.NewScheduler(time.Local),
+	}
+	gs.TagsUnique()
+	return gs
+}
+
+func (s *cronScheduler)LetJobs(c *gin.Context) {
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"result": listCronJobs(s),
+	})
+}
+
+func (s *cronScheduler)AddJob(c *gin.Context) {
+	body := payload{}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest,
+			gin.H{
+				"error": err.Error(),
+				"message": "Invalid inputs, please check your inputs",
+			})
+		return
+	}
+
+	job, err := addCronJobs(s, body.CronJob, body.TaskName, func () {fmt.Println("funciona!")}) //TODO function handler
+	if err != nil {
+
+		c.AbortWithStatusJSON(http.StatusInternalServerError, map[string]interface{}{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusAccepted, map[string]interface{}{
+		"isRunningNow": job.IsRunning(),
+		"name": job.Tags(),
+		"next run": job.NextRun(),
+	})
+	
+}
+
+func (s *cronScheduler)DeleteJob(c *gin.Context) {
+	jobName := c.Param("jobname")
+
+	deleteCronJob(s, jobName)
+	c.AbortWithStatusJSON(http.StatusOK, map[string]interface{}{
+		"result": listCronJobs(s),
+	})
+
 }
